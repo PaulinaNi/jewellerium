aaf1bf3b5a1a498343ad61fcc22f18e335a1d1e4
Paulina N <paulina.justyna.niedzielska@gmail.com>
2023-04-22 21:54:16 +0100

    start with hero section

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,10 +1,14 @@
 import './homepage.styles.css'
 
-
 export default function Homepage() {
  return (
   <section>
-   <h1>Homepage</h1>
+
+   <div className='hero'>
+    <h1>Beautiful & Statement Jewellery</h1>
+    <h2>- designed and handmade in London -</h2>
+   </div>
+   
   </section>
  )
 }
